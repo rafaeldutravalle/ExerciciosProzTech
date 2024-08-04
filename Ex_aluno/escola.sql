752788ca016d45e879ec8f3871a13442e0725b4c
Rafael Valle <raf_valle@hotmail.com>
2024-08-04 03:37:19 -0300

    banco escola

---
+++ Ex_aluno/escola.sql
@@ -0,0 +1,10 @@
+CREATE TABLE ALUNO (
+    ID INT PRIMARY KEY,
+    NOME VARCHAR(100),
+    EMAIL VARCHAR(100),
+    ENDERECO VARCHAR(200)
+);
+INSERT INTO ALUNO (ID, NOME, EMAIL, ENDERECO)
+VALUES (1, 'João da Silva', 'joao@email.com', 'Rua dos Bobos, 0');
+INSERT INTO ALUNO (ID, NOME, EMAIL, ENDERECO)
+VALUES (2, 'Maria Souza', 'maria@email.com', 'Avenida das Flores, 123');
